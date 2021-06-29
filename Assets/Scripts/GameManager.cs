6be25c67dae4f4462f04f36b4a8e78289dba9a31
Disastercake <mike@disastercake.com>
2021-06-29 07:23:24 -0400

    Create GameManager.cs
    Will act as a singleton that holds references to various game components, including serializable data, for other game elements to easily reference.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; } = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("There was more than one GameManager component in the scene.  There can only be one!  Slaying the weakest now...");
+            Destroy(this); // Only destroy component in case the GameObject it's attached to is important.
+        }
+    }
+
+    public GameSaveData _GameSaveData { get; private set; } = null;
+    public void SetGameSaveData(GameSaveData data)
+    {
+        _GameSaveData = null;
+    }
+}
