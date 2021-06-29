e8821e4b9b37d0f97bba45d0414f7426858667e5
Disastercake <mike@disastercake.com>
2021-06-29 07:22:07 -0400

    Changed GameData to GameSaveData
    Renamed class to GameSaveData to be more descriptive.  Also removed a namespace left in when migrating after from another project.

--- Assets/Scripts/GameSaveData.cs
+++ Assets/Scripts/GameSaveData.cs
@@ -1,21 +1,18 @@
 ﻿using System.Collections.Generic;
 
-namespace SoulSaga
+/// <summary>
+/// All persistent game data that is saved to a savefile.
+/// For use with GameManager and FileManager.  All game logic should go through GameManager to get or set this information.
+/// </summary>
+[System.Serializable]
+public class GameSaveData
 {
-    /// <summary>
-    /// All persistent game data that is saved to a savefile.
-    /// For use with GameManager and FileManager.  All game logic should go through GameManager to get or set this information.
-    /// </summary>
-    [System.Serializable]
-    public class GameData
-    {
-        [Newtonsoft.Json.JsonProperty]
-        public string PlayerName = string.Empty;
+    [Newtonsoft.Json.JsonProperty]
+    public string PlayerName = string.Empty;
 
-        [Newtonsoft.Json.JsonProperty]
-        public Dictionary<int, int> PlayerInventory = new Dictionary<int, int>();
+    [Newtonsoft.Json.JsonProperty]
+    public Dictionary<int, int> PlayerInventory = new Dictionary<int, int>();
 
-        [Newtonsoft.Json.JsonProperty]
-        public int PlayerGold = 0;
-    }
+    [Newtonsoft.Json.JsonProperty]
+    public int PlayerGold = 0;
 }
