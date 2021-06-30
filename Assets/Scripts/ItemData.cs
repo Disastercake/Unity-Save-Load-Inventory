4d04b254dd39dd47df8b13d5da6ce8d52c5393a5
Disastercake <mike@disastercake.com>
2021-06-30 08:08:14 -0400

    Create ItemData.cs

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData
+{
+    public string Id { get; private set; } = string.Empty;
+    public string Name { get; private set; } = string.Empty;
+    public string Description { get; private set; } = string.Empty;
+
+    public ItemData(string id, string name, string description)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+    }
+}
