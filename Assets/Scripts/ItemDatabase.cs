016c40bc70611dd99b10a820b31c3a526459c48d
Disastercake <mike@disastercake.com>
2021-06-30 15:53:18 -0400

    Update ItemDatabase.cs

--- Assets/Scripts/ItemDatabase.cs
+++ Assets/Scripts/ItemDatabase.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public static class ItemDatabase
@@ -26,5 +27,24 @@ public static class ItemDatabase
 
         id = "potion03";
         _database.Add(id, new ItemData(id, "Purple Potion", "A purple concotion with a bitter taste."));
+    }
+
+    public static Dictionary<string, int> GetRandomItems(int num)
+    {
+        Dictionary<string, int> items = new Dictionary<string, int>();
+
+        var keys = _database.Keys.ToArray();
+
+        for (int i = 0; i < num; i++)
+        {
+            var id = keys[Random.Range(0, keys.Length - 1)];
+
+            if (items.ContainsKey(id))
+                items[id] = items[id] + 1;
+            else
+                items.Add(id, 1);
+        }
+
+        return items;
     }
 }
