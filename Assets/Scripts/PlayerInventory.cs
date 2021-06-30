bacc9fa806ea2af2660799b1f34adafa421d612b
Disastercake <mike@disastercake.com>
2021-06-30 15:53:22 -0400

    Update PlayerInventory.cs

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -2,17 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class PlayerInventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private ItemScrollView _ItemScrollView = null;
+
+    private List<ItemData> _itemList = null;
+
+    private void OnEnable()
     {
-        
+        try { Messenger.AddListener(Messages.GameLoaded, UpdatePlayerInventory); } catch { }
+        try { Messenger.AddListener(Messages.PlayerInventoryChanged, UpdatePlayerInventory); } catch { }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        try { Messenger.RemoveListener(Messages.GameLoaded, UpdatePlayerInventory); } catch { }
+        try { Messenger.RemoveListener(Messages.PlayerInventoryChanged, UpdatePlayerInventory); } catch { }
     }
+
+    private void OnDestroy()
+    {
+        try { Messenger.RemoveListener(Messages.GameLoaded, UpdatePlayerInventory); } catch { }
+        try { Messenger.RemoveListener(Messages.PlayerInventoryChanged, UpdatePlayerInventory); } catch { }
+    }
+
+    private void UpdatePlayerInventory()
+    {
+        var inventory = GameManager.Instance._GameSaveData.PlayerInventory;
+
+        //for (int i = 0; i < length; i++)
+        //{
+
+        //}
+    }
+
+    private void SetList(List<ItemData> itemList)
+    {
+        _itemList = itemList;
+
+        //_ItemScrollView.SetList(_itemList);
+    }
+
+
+
 }
