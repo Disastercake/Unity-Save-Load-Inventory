b9af43e67f3fd29fd4405bcc070983d75b0e291d
Disastercake <mike@disastercake.com>
2021-06-30 14:21:29 -0400

    Create ButtonSaveGame.cs

---
+++ Assets/Scripts/ButtonSaveGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class ButtonSaveGame : MonoBehaviour
+{
+    public void OnClick()
+    {
+        if (FileManager.TrySave(GameManager.Instance._GameSaveData))
+        {
+            Debug.Log("Save SUCCESS.");
+            try { Messenger.Broadcast(Messages.GameSaved); } catch { }
+        }
+        else
+        {
+            Debug.Log("Save FAIL.");
+        }
+    }
+}
