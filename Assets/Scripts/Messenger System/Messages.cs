d14cfc574711685c2b81a9563490e4294694fe77
Disastercake <mike@disastercake.com>
2021-06-30 14:18:34 -0400

    Create Messages.cs
    Helper method for utilizing code hinting to prevent typos in string messages.

---
+++ Assets/Scripts/Messenger System/Messages.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// Helper class to easily track messages used with the Messenger system.
+/// </summary>
+public static class Messages
+{
+    /// <summary>
+    /// Broadcast when the game has been saved.
+    /// </summary>
+    public const string GameSaved = "GameSaved";
+
+    /// <summary>
+    /// Broadcast when the game has been loaded.
+    /// </summary>
+    public const string GameLoaded = "GameLoaded";
+}
