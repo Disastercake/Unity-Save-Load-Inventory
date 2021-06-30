4622f09d79d50fee9d053423d20932ddf225cb31
Disastercake <mike@disastercake.com>
2021-06-30 14:19:45 -0400

    Update StartMenu.cs
    Added disallow multiple components attribute.

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class StartMenu : MonoBehaviour
 {
     // Start is called before the first frame update
